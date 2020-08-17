fb2c7506ecb0ec580f6dec344549474a9984a68d
Oscar Benedito <oscar@oscarbenedito.com>
2020-08-17 19:51:30 +0200

    Merge post-hugo-script into deploy-website

--- deploy-website.sh
+++ deploy-website.sh
@@ -21,15 +21,38 @@ WEB_PATH="/srv/oscarbenedito.com"
 GOTIFY_DOMAIN="gotify.oscarbenedito.com"
 FILE_DIR="$(dirname "$(realpath "$0")")"
 
+# Pull rewritting history if needed, check that commit is PGP signed by a known
+# key and if so, rebuild the website
 git -C $HUGO_PATH fetch origin master
 git -C $HUGO_PATH reset --hard origin/master
 git -C $HUGO_PATH verify-commit master || exit 1
 rm -rf $HUGO_PATH/public
 rm -rf $HUGO_PATH/resources
 hugo -s $HUGO_PATH --gc
-$FILE_DIR/post-hugo-script.py "$FILE_DIR/post_hugo_script.json"
+
+# Edit Hugo output: delete unwanted files (also from sitemap)
+rm -rf "$HUGO_PATH/public/licenses/page/"
+rm -f "$HUGO_PATH/public/index.xml" "$HUGO_PATH/public/licenses/index.html" \
+    "$HUGO_PATH/public/licenses/index.xml"
+path="\\/licenses\\/"
+sed -i "/<url>/{:a;N;/<\/url>/!ba};N;/<loc>https:\/\/oscarbenedito\.com${path}<\/loc>/d" \
+    "$HUGO_PATH/public/sitemap.xml"
+# Explanation of RegEx:
+# /<url>/       # find <url>
+# {             # start command declaration
+#   :a          # create label "a"
+#   N           # read next line into pattern space
+#   /<\/url>/!  # if not match </url>...
+#   ba          # goto "a"
+# }             # end command
+# N             # read next line into pattern space (the empty line on sitemap.xml)
+# /<lo....oc>/  # if pattern matches...
+# d             # delete
+
+# Sync new build to production
 rsync --perms --recursive --checksum --delete "$HUGO_PATH/public/" "$WEB_PATH"
 
+# Notify
 API_TOKEN="$(cat "$FILE_DIR/gotify_token.txt")"
 TITLE="Web update triggered"
 MESSAGE="Git hooks triggered an update of the website."
