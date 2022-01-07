0464d076c0d7e72a7a1342be81e41e86ef3ba026
Oscar Benedito <oscar@oscarbenedito.com>
2022-01-07 20:56:48 +0100

    Add check-disk-usage

---
+++ check-disk-usage.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# 2022 Oscar Benedito <oscar@oscarbenedito.com>
+# License: CC0 1.0 Universal License
+
+tmp="$(df -Ph | grep ' /$')"
+used="$(echo "$tmp" | awk {'print $5'})"
+left="$(echo "$tmp" | awk {'print $4'})"
+max=80
+
+if [ "${used%?}" -ge "${max}" ]; then
+    notify "Disk usage warning" "$(hostname) has used $used of its disk space. Space left: $left."
+fi
