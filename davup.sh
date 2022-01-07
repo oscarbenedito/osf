5bcaa9b2e1de15bf43a9d947d4e2619d5a8f94b7
Oscar Benedito <oscar@oscarbenedito.com>
2022-01-07 20:56:28 +0100

    Shorten copyright notices and move some to CC0

--- davup.sh
+++ davup.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
-# DAVup
 # Copyright (C) 2020 Oscar Benedito <oscar@oscarbenedito.com>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# License: Affero General Public License version 3 or later
 
-# Back up calendars and contacts from a DAV server (CalDAV and CardDAV).
+# DAVup: back up calendars and contacts from a DAV server (CalDAV and CardDAV).
 
 domain="<DAV server address>"       # example: https://dav.mailbox.org (no trailing "/")
 user="<username>"
--- notify.sh
+++ notify.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
-# Gotify notify
-# Copyright (C) 2020 Oscar Benedito <oscar@oscarbenedito.com>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# 2020 Oscar Benedito <oscar@oscarbenedito.com>
+# License: CC0 1.0 Universal License
 
-# Notifies user using a Gotify instance.
+# Gotify notify: notifies user using a Gotify instance.
 
 GOTIFY_DOMAIN="<redacted>"
 GOTIFY_TOKEN="<redacted>"
